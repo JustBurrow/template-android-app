a77797d4fcf3d09d52a724f7f9489498dee2e607
Just Burrow <just.burrow@lul.kr>
2024-09-15 13:50:49 +0900

    ➕ Compose Navigation 추가.

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     implementation(libs.androidx.core)
     implementation(libs.hilt)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
